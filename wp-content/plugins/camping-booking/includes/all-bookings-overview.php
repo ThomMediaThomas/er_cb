f804cd66ad346ae6fe521556fbf39563012a70f9
Thomas <thomas.bartels@nos.nl>
2018-07-29 13:43:18 +0100

    Added more booking-functionality

--- wp-content/plugins/camping-booking/includes/all-bookings-overview.php
+++ wp-content/plugins/camping-booking/includes/all-bookings-overview.php
@@ -3,6 +3,7 @@
  function add_acf_columns ($columns) {
    unset($columns['date']);
    return array_merge ($columns, array ( 
+     'booking_type' => __('Type verblijf'),
      'fullname' => __('Name'),
      'date_from' => __('Van'),
      'date_to' => __('Tot'),
@@ -17,6 +18,18 @@
  			$name = get_post_meta($post_id, 'first_name', true) . ' ' . get_post_meta($post_id, 'name', true);
  			echo $name;
  		break;
+ 		case 'booking_type':
+ 			$type = get_post_meta($post_id, 'type', true);
+ 			$friendlyType = '';
+
+ 			if ($type == 'chalet') {
+ 				$friendlyType = getFieldSelectLabel('type', $post_id) . ': ' . getFieldSelectLabel('chalet_type', $post_id);
+ 			} else {
+ 				$friendlyType = getFieldSelectLabel('type', $post_id) . ': ' . getFieldSelectLabel('camping_type', $post_id);
+ 			}
+
+ 			echo $friendlyType;
+ 		break;
  		default:
  			echo get_post_meta($post_id, $column, true);
  		break;	
@@ -24,3 +37,11 @@
    
  }
  add_action ('manage_booking_posts_custom_column', 'exhibition_custom_column', 10, 2);
+
+ function getFieldSelectLabel($field, $post_id)
+ {
+	$field = get_field_object($field, $post_id);
+	$value = $field['value'];
+	$label = $field['choices'][ $value ];
+	return $label;
+ }
