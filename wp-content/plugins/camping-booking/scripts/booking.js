77a2789a82fb01b0bc57e40d3613702e6dba419f
Thomas <thomas.bartels@nos.nl>
2018-08-06 20:54:18 +0100

    Added button to reload manuayly + added types

--- wp-content/plugins/camping-booking/scripts/booking.js
+++ wp-content/plugins/camping-booking/scripts/booking.js
@@ -7,36 +7,49 @@ jQuery(document).ready(function ($) {
 	}
 
 	//add logic for assign-box
-	var $assignedSelect = $('#acf-field-assigned'),
-		assignables = [];
+	var $assignedSelect = $('#acf-field-assigned');
 
-	$assignedSelect.find('option').each(function(){
-		if ($(this).val() != 'null') {
-		    assignables.push(parseInt($(this).val()));
-		}
-	});
+	if (isBookingPage) {
+		$assignedSelect.after('<a href="#" class="button" id="reload-assign-select">Beschikbare plaatsen herladen</a>');
 
-	if (isBookingPage && $assignedSelect.length > 0) {
+		$('#reload-assign-select').on("click", function () {
+			updateSelect($assignedSelect);
+			return false;
+		});
+
 		$assignedSelect.attr('disabled', 'disabled');
 
 		setTimeout(function () {
-			var dateFrom = $('#acf-date_from').find('input.hasDatepicker').val(),
-				dateTo = $('#acf-date_to').find('input.hasDatepicker').val();
-
-			console.log('/wp-json/camping-booking/assigned_accomodations?date_from=' + dateFrom + '&date_to=' + dateTo);
-
-			$.get('/wp-json/camping-booking/assigned_accomodations?date_from=' + dateFrom + '&date_to=' + dateTo, {}, function (accomodations) {
-				$assignedSelect.find('option').each(function(){
-					var $this = $(this),
-					parsedValue = parseInt($this.val());
-					if ((accomodations.indexOf(parsedValue) > -1)) {
-						$this.attr('disabled', 'disabled');
-					}else{
-						$this.removeAttr('disabled');
-					}
-				});
-				$assignedSelect.removeAttr('disabled');
-			});
+			updateSelect($assignedSelect);
 		}, 1000);
 	}
 });
+
+function updateSelect($assignedSelect)
+{
+	$assignedSelect.attr('disabled', 'disabled');
+
+	var dateFrom = $('#acf-date_from').find('input.hasDatepicker').val(),
+		dateTo = $('#acf-date_to').find('input.hasDatepicker').val(),
+		type = $('select#acf-field-type').val(),
+		subType = type == 'chalet' ? $('#acf-field-chalet_type').val() : $('#acf-field-camping_type').val();
+
+	$.get(createUrl(dateFrom, dateTo, type, subType), {}, function (accomodations) {
+		$assignedSelect.find('option').each(function(){
+			var $this = $(this),
+			parsedValue = parseInt($this.val());
+			if ((accomodations.indexOf(parsedValue) > -1)) {
+				$this.attr('disabled', 'disabled');
+			}else{
+				$this.removeAttr('disabled');
+			}
+		});
+		$assignedSelect.removeAttr('disabled');
+	});
+
+}
+
+function createUrl (dateFrom, dateTo, type, subType)
+{
+	return '/wp-json/camping-booking/assigned_accomodations?date_from=' + dateFrom + '&date_to=' + dateTo + '&type=' + type + '&subtype=' + subType;
+}
